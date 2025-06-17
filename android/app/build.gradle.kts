245a39907ac6f1fa06b7585c754a8e6962dc4268
kawserfdev <kawserahmed5459@gmail.com>
2025-06-17 22:48:51 +0600

    Version controling solved

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,13 @@ plugins {
 
 android {
     namespace = "com.example.islamia"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion =  "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -24,10 +25,10 @@ android {
         applicationId = "com.example.islamia"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
@@ -41,4 +42,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4") // Required for flutter_local_notifications
 }
